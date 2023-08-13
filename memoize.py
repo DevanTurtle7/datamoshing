07caf0b319a123a9716211994b06afb67fd58f1c
Devan Kavalchek <devankav7@gmail.com>
2023-08-13 03:06:10 -0400

    Refactor vars

--- memoize.py
+++ memoize.py
@@ -2,35 +2,38 @@ from moviepy.editor import *
 import numpy as np
 from util.pixels import *
 
-THRESHOLD = 100
-prev = []
+THRESHOLD = 200
+prev_render = []
 
 def make_frame(t, clip):
-  global prev
+  global prev_render
   current = clip.get_frame(t)
+  time_since_last = 1/clip.fps
 
   if (t == 0):
-    prev = current
+    prev_render = current
     return current
     
   frame = []
+  prev_frame = clip.get_frame(t - (time_since_last * 2))
 
   for y in range(0, clip.h):
     row = []
     for x in range(0, clip.w):
-      prev_pixel = prev[y][x]
+      prev_render_pixel = prev_render[y][x]
+      prev_frame_pixel = prev_frame[y][x]
       current_pixel = current[y][x]
-      diff = color_diff(prev_pixel, current_pixel)
+      diff = color_diff(prev_frame_pixel, current_pixel)
 
-      if diff < THRESHOLD:
-        row.append(prev_pixel)
-      else:
+      if diff > THRESHOLD:
         row.append(current_pixel)
+      else:
+        row.append(prev_render_pixel)
 
     frame.append(row)
 
-  prev = np.array(frame)
-  return prev
+  prev_render = np.array(frame)
+  return prev_render
 
 # 1: 31, 33
 # 2: 176, 178
